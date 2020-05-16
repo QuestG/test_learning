14fbcb7995bdce00960de5bc7685401d5013268a
guisuo <guisuo@huami.com>
2020-05-16 22:57:33 +0800

    feat:  学习robolectric

--- robolectric_tutorial/src/androidTest/java/com/learn/robolectric_tutorial/WelcomeActivityAndroidTest.kt
+++ robolectric_tutorial/src/androidTest/java/com/learn/robolectric_tutorial/WelcomeActivityAndroidTest.kt
@@ -3,15 +3,20 @@ package com.learn.robolectric_tutorial
 import android.content.Intent
 import android.widget.Button
 import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.robolectric.Shadows
 
 /**
  * @author quest
  * @date 2020/5/12
- * Description: trueth只能在单元测试引用，无法在instrument test中使用
+ * Description: truth只能在单元测试引用，无法在instrument test中使用
+ *
+ * 对于Fragment，androidx提供了FragmentScenario API来进行测试。
  */
+@RunWith(AndroidJUnit4::class)
 class WelcomeActivityAndroidTest {
 
     @Test
--- robolectric_tutorial/src/test/java/com/learn/robolectric_tutorial/WelcomeActivityTest.kt
+++ robolectric_tutorial/src/test/java/com/learn/robolectric_tutorial/WelcomeActivityTest.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Build
 import android.widget.Button
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
@@ -16,12 +17,30 @@ import org.robolectric.annotation.Config
  * @author quest
  * @date 2020/5/12
  * Description: RobolectricTestRunner只能用于单元测试，即只能在test/java目录下才能被引用
+ * 这里就是纯使用Robolectric的api，不过更推荐使用AndroidX Test APIs
  *
- * 注解@Config中可以为Robolectric指定测试的sdk
+ * 比如RuntimeEnvironment.application 用ApplicationProvider来替代等；
+ *
+ * Robolectric中关于View的api非常少，仅限于使用findViewById。一般情况下，对于instrumentation tests还是建议使用Espresso。
+ * Robolectric 在4.0之后，支持Espresso API。
+ *
+ * 注解@Config中可以为Robolectric指定测试的sdk，还可以指定类、方法、资源目录等，可以参考@Config的源码。
+ *
+ * 如果想将配置想集中，可以在module中合适的目录下（通常是src/test/resources）创建robolectric.properties文件。
+ *
+ * 如果需要改变默认的配置，可以创建RobolectricTestRunner子类，充血buildGlobalConfig方法，然后在@Runwith中指定这个子类。
+ *
+ *
+ *
  */
-@Config(sdk = [Build.VERSION_CODES.P])
+@Config(sdk = [Build.VERSION_CODES.P], shadows = [])
 @RunWith(RobolectricTestRunner::class)
 class WelcomeActivityTest {
+
+    @Before
+    fun setup() {
+    }
+
     @Test
     fun clickingLogin_shouldStartLoginActivity() {
         val activity = Robolectric.setupActivity(WelcomeActivity::class.java)
